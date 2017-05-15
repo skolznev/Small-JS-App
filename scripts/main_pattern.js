504b9987a14976f26318cade36089f80c57bd720
Dmitry <skolznevdm@gmail.com>
2017-05-15 17:18:23 +0300

    0.1.8

---
+++ scripts/main_pattern.js
@@ -0,0 +1,38 @@
+var map = AmCharts.makeChart( "chartdiv", {
+  type: "map",
+  theme: "light",
+  dataProvider: {
+    map: "worldHigh",
+    zoomLevel: 2.5,
+    zoomLongitude: 20,
+    zoomLatitude: 0,
+    areas: [ {
+        id: "MZ",
+        color: "#f00"
+      }, {
+        id: "CD",
+        color: "#f00"
+      }, {
+        id: "ET",
+        color: "#f00"
+      }, {
+        id: "MA",
+        color: "#f00"
+      }
+    ]
+  },
+
+  areasSettings: {
+    rollOverOutlineColor: "#FFFFFF",
+    rollOverColor: "#CC0000",
+    alpha: 0.8,
+    unlistedAreasAlpha: 1, //0 - не показывать остальные страны
+    // unlistedAreasColor: "#8dd9ef" //Цвет заблокированных стран
+  }
+
+} );
+
+//Вызов модального окна помощника
+$(document).ready(function() {
+  $("#helperTask").modal('show');
+});
