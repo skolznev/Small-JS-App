d30c425d5316b9c48f6d1a88e429a49af43b9292
Dmitry <skolznevdm@gmail.com>
2017-05-15 19:55:52 +0300

    0.1.9

--- scripts/main_pattern.js
+++ scripts/main_pattern.js
@@ -36,3 +36,16 @@ var map = AmCharts.makeChart( "chartdiv", {
 $(document).ready(function() {
   $("#helperTask").modal('show');
 });
+
+//Счетчик монет
+var valueCoin = document.getElementById('count_balance').innerHTML;
+var value = document.getElementById('count_balance');
+count = 0;
+var updateTimer = function() {
+  if (count == valueCoin) {
+    return;
+  } else {
+    value.innerHTML = count += 1;
+  }
+};
+setInterval(updateTimer, 8);
